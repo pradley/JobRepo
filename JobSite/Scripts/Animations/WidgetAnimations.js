451f16f8ec9217a460d9fdf7126904703d6c40bb
Pat <fattalist@hotmail.com>
2018-03-16 13:53:57 -0700

    Working on an animation for the widget page. Did some styling on the landing page and made the buttons spacing better

---
+++ JobSite/Scripts/Animations/WidgetAnimations.js
@@ -0,0 +1,37 @@
+﻿$(function () {
+    $('[class=box]').each(function () {
+        $(this).hide();
+    });
+
+    $(".animation").click(function () {
+        var appearSpeed = 2000;
+        var removeSpeed = 1000;
+        $('[class=box]').each(function () {
+            $(this).fadeIn(appearSpeed);
+            $(this).animate(
+                {
+                    border: "25px",
+                    opacity: "0.0",
+                },
+                appearSpeed
+
+
+            )
+            appearSpeed = appearSpeed + 1000;
+            removeSpeed = removeSpeed - 100;
+        })
+
+        setTimeout(function () {
+            $('[class=box]').each(function () {
+
+               // $(this).fadeOut();
+            })
+        });
+
+    });
+
+
+
+
+
+})
